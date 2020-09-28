f6ef61a0b95b05a9d059a8c5ddf033db0605baa9
MHenrie <silvamarlon87@gmail.com>
2020-09-27 23:21:05 -0300

    config database

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,5 @@
-require('dotenv').config()
-
 module.exports = {
-  dialect: process.env.DB_DIALECT,
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  dialect: 'sqlite',
   storage: './src/database/storage.sqlite',
   define: {
     timestamps: true,
